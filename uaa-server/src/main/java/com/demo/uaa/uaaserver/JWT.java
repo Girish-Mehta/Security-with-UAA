e1508a0f892cca1d508c6962895a4db6c0937afa
a3loko <lokesh09rishi@gmail.com>
2018-04-04 22:50:36 +0530

    Authentication at UAA completed!

--- uaa-server/src/main/java/com/demo/uaa/uaaserver/JWT.java
+++ uaa-server/src/main/java/com/demo/uaa/uaaserver/JWT.java
@@ -1,23 +1,41 @@
 package com.demo.uaa.uaaserver;
 
+import java.security.Key;
 import java.util.Arrays;
 import java.util.List;
 
+import org.jose4j.jwa.AlgorithmConstraints;
+import org.jose4j.jwa.AlgorithmConstraints.ConstraintType;
+import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
+import org.jose4j.jwe.JsonWebEncryption;
+import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
+import org.jose4j.jwk.JsonWebKey;
 import org.jose4j.jwk.RsaJsonWebKey;
 import org.jose4j.jwk.RsaJwkGenerator;
 import org.jose4j.jws.AlgorithmIdentifiers;
 import org.jose4j.jws.JsonWebSignature;
 import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.consumer.ErrorCodes;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.lang.InvalidAlgorithmException;
 import org.jose4j.lang.JoseException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JWT {
-	
-	@GetMapping("/jwt")
-	public void createJwtToken() {
+
+	@GetMapping("/jwt/{user}")
+	public String createJwtToken(@PathVariable String user) {
 		RsaJsonWebKey rsaJsonWebKey = null;
+		JsonWebKey jwk = null;
+
+		// Create a new Json Web Encryption object
+		JsonWebEncryption senderJwe = new JsonWebEncryption();
 		try {
 			rsaJsonWebKey = RsaJwkGenerator.generateJwk(2048);
 		} catch (JoseException e1) {
@@ -25,114 +43,105 @@ public class JWT {
 			e1.printStackTrace();
 		}
 
-	    // Give the JWK a Key ID (kid), which is just the polite thing to do
-	    rsaJsonWebKey.setKeyId("k1");
+		// Give the JWK a Key ID (kid), which is just the polite thing to do
+		rsaJsonWebKey.setKeyId("k1");
 
-	    // Create the Claims, which will be the content of the JWT
-	    JwtClaims claims = new JwtClaims();
-	    claims.setIssuer("Issuer");  // who creates the token and signs it
-	    claims.setAudience("Audience"); // to whom the token is intended to be sent
-	    claims.setExpirationTimeMinutesInTheFuture(10); // time when the token will expire (10 minutes from now)
-	    claims.setGeneratedJwtId(); // a unique identifier for the token
-	    claims.setIssuedAtToNow();  // when the token was issued/created (now)
-	    claims.setNotBeforeMinutesInThePast(2); // time before which the token is not yet valid (2 minutes ago)
-	    claims.setSubject("9468075105"); // the subject/principal is whom the token is about
-	    claims.setClaim("username","9468075105"); // additional claims/attributes about the subject can be added
-	    List<String> groups = Arrays.asList("customer", "guest", "farmer");
-	    claims.setStringListClaim("guest", groups); // multi-valued claims work too and will end up as a JSON array
+		// Create the Claims, which will be the content of the JWT
+		JwtClaims claims = new JwtClaims();
+		claims.setIssuer("Issuer"); // who creates the token and signs it
+		claims.setAudience("Audience"); // to whom the token is intended to be sent
+		claims.setExpirationTimeMinutesInTheFuture(10); // time when the token will expire (10 minutes from now)
+		claims.setGeneratedJwtId(); // a unique identifier for the token
+		claims.setIssuedAtToNow(); // when the token was issued/created (now)
+		claims.setNotBeforeMinutesInThePast(2); // time before which the token is not yet valid (2 minutes ago)
+		claims.setSubject(user); // the subject/principal is whom the token is about
+		claims.setClaim("username", "9468075105"); // additional claims/attributes about the subject can be added
+		List<String> groups = Arrays.asList("customer", "guest", "farmer");
+		claims.setStringListClaim("guest", groups); // multi-valued claims work too and will end up as a JSON array
 
-	    // A JWT is a JWS and/or a JWE with JSON claims as the payload.
-	    // In this example it is a JWS so we create a JsonWebSignature object.
-	    JsonWebSignature jws = new JsonWebSignature();
+		// A JWT is a JWS and/or a JWE with JSON claims as the payload.
+		// In this example it is a JWS so we create a JsonWebSignature object.
+		JsonWebSignature jws = new JsonWebSignature();
 
-	    // The payload of the JWS is JSON content of the JWT Claims
-	    jws.setPayload(claims.toJson());
+		// The payload of the JWS is JSON content of the JWT Claims
+		jws.setPayload(claims.toJson());
 
-	    // The JWT is signed using the private key
-	    jws.setKey(rsaJsonWebKey.getPrivateKey());
+		// The JWT is signed using the private key
+		jws.setKey(rsaJsonWebKey.getPrivateKey());
 
-	    // Set the Key ID (kid) header because it's just the polite thing to do.
-	    // We only have one key in this example but a using a Key ID helps
-	    // facilitate a smooth key rollover process
-	    jws.setKeyIdHeaderValue(rsaJsonWebKey.getKeyId());
+		// Set the Key ID (kid) header because it's just the polite thing to do.
+		// We only have one key in this example but a using a Key ID helps
+		// facilitate a smooth key rollover process
+		jws.setKeyIdHeaderValue(rsaJsonWebKey.getKeyId());
 
-	    // Set the signature algorithm on the JWT/JWS that will integrity protect the claims
-	    jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
-
-	    // Sign the JWS and produce the compact serialization or the complete JWT/JWS
-	    // representation, which is a string consisting of three dot ('.') separated
-	    // base64url-encoded parts in the form Header.Payload.Signature
-	    // If you wanted to encrypt it, you can simply set this jwt as the payload
-	    // of a JsonWebEncryption object and set the cty (Content Type) header to "jwt".
-	    String jwt = null;
+		// Set the signature algorithm on the JWT/JWS that will integrity protect the
+		// claims
+		jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+		
+		// Sign the JWS and produce the compact serialization or the complete JWT/JWS
+		// representation, which is a string consisting of three dot ('.') separated
+		// base64url-encoded parts in the form Header.Payload.Signature
+		// If you wanted to encrypt it, you can simply set this jwt as the payload
+		// of a JsonWebEncryption object and set the cty (Content Type) header to "jwt".
+		String jwt = null;
+		String jwkJson = null;
 		try {
 			jwt = jws.getCompactSerialization();
+			// The shared secret or shared symmetric key represented as a octet sequence
+			// JSON Web Key (JWK)
+			jwkJson = "{\"kty\":\"oct\",\"k\":\"Fdh9u8rINxfivbrianbbVT1u232VQBZYKx1HGAGPt2I\"}";
+			jwk = JsonWebKey.Factory.newJwk(jwkJson);
 		} catch (JoseException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
-
-	    // Now you can do something with the JWT. Like send it to some other party
-	    // over the clouds and through the interwebs.
-	    System.out.println("JWT: " + jwt);
-
-//
-//	    // Use JwtConsumerBuilder to construct an appropriate JwtConsumer, which will
-//	    // be used to validate and process the JWT.
-//	    // The specific validation requirements for a JWT are context dependent, however,
-//	    // it typically advisable to require a (reasonable) expiration time, a trusted issuer, and
-//	    // and audience that identifies your system as the intended recipient.
-//	    // If the JWT is encrypted too, you need only provide a decryption key or
-//	    // decryption key resolver to the builder.
-//	    JwtConsumer jwtConsumer = new JwtConsumerBuilder()
-//	            .setRequireExpirationTime() // the JWT must have an expiration time
-//	            .setAllowedClockSkewInSeconds(30) // allow some leeway in validating time based claims to account for clock skew
-//	            .setRequireSubject() // the JWT must have a subject claim
-//	            .setExpectedIssuer("Issuer") // whom the JWT needs to have been issued by
-//	            .setExpectedAudience("Audience") // to whom the JWT is intended for
-//	            .setVerificationKey(rsaJsonWebKey.getKey()) // verify the signature with the public key
-//	            .setJweAlgorithmConstraints( // only allow the expected signature algorithm(s) in the given context
-//	                    new AlgorithmConstraints(ConstraintType.WHITELIST, // which is only RS256 here
-//	                            AlgorithmIdentifiers.RSA_USING_SHA256))
-//	            .build(); // create the JwtConsumer instance
-//	    try
-//	    {
-//	        //  Validate the JWT and process it to the Claims
-//	        JwtClaims jwtClaims = jwtConsumer.processToClaims(jwt);
-//	        System.out.println("JWT validation succeeded! " + jwtClaims);
-//	    }
-//	    catch (InvalidJwtException e)
-//	    {
-//	        // InvalidJwtException will be thrown, if the JWT failed processing or validation in anyway.
-//	        // Hopefully with meaningful explanations(s) about what went wrong.
-//	        System.out.println("Invalid JWT! " + e);
-//
-//	        // Programmatic access to (some) specific reasons for JWT invalidity is also possible
-//	        // should you want different error handling behavior for certain conditions.
-//
-//	        // Whether or not the JWT has expired being one common reason for invalidity
-//	        if (e.hasExpired())
-//	        {
-//	            try {
-//					System.out.println("JWT expired at " + e.getJwtContext().getJwtClaims().getExpirationTime());
-//				} catch (MalformedClaimException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
-//	        }
-//
-//	        // Or maybe the audience was invalid
-//	        if (e.hasErrorCode(ErrorCodes.AUDIENCE_INVALID))
-//	        {
-//	            try {
-//					System.out.println("JWT had wrong audience: " + e.getJwtContext().getJwtClaims().getAudience());
-//				} catch (MalformedClaimException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
-//	        }
-//	    }
+		// A JSON string with only the public key info
+        String publicKeyJwkString = rsaJsonWebKey.toJson(JsonWebKey.OutputControlLevel.PUBLIC_ONLY);
+		senderJwe.setPlaintext(publicKeyJwkString + "\nJWT:" + jwt);
+		System.out.println("New String:"+publicKeyJwkString);
 		
+		// The plaintext of the JWE is the message that we want to encrypt.
+//		senderJwe.setPlaintext(rsaJsonWebKey.getKey().toString() + "\nJWT:" + jwt);
+
+		
+		// Set the "alg" header, which indicates the key management mode for this JWE.
+		// In this example we are using the direct key management mode, which means
+		// the given key will be used directly as the content encryption key.
+		senderJwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.DIRECT);
+		
+		// Set the "enc" header, which indicates the content encryption algorithm to be
+		// used.
+		// This example is using AES_128_CBC_HMAC_SHA_256 which is a composition of AES
+		// CBC
+		// and HMAC SHA2 that provides authenticated encryption.
+		senderJwe.setEncryptionMethodHeaderParameter(ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256);
+	
+		// Set the key on the JWE. In this case, using direct mode, the key will used
+		// directly as
+		// the content encryption key. AES_128_CBC_HMAC_SHA_256, which is being used to
+		// encrypt the
+		// content requires a 256 bit key.
+		senderJwe.setKey(jwk.getKey());
+
+		// Produce the JWE compact serialization, which is where the actual encryption
+		// is done.
+		// The JWE compact serialization consists of five base64url encoded parts
+		// combined with a dot ('.') character in the general format of
+		// <header>.<encrypted key>.<initialization vector>.<ciphertext>.<authentication
+		// tag>
+		// Direct encryption doesn't use an encrypted key so that field will be an empty
+		// string
+		// in this case.
+		String compactSerialization = null;
+		try {
+			compactSerialization = senderJwe.getCompactSerialization();
+		} catch (JoseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		System.out.println("Compact:"+compactSerialization);
+		return compactSerialization;
 	}
 }
